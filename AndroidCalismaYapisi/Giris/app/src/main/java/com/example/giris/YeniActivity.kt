d9192045f2f7e17c9e3d8713051edb42db0b1504
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2023-02-02 16:45:33 +0300

    Kotlin 1.2

---
+++ AndroidCalismaYapisi/Giris/app/src/main/java/com/example/giris/YeniActivity.kt
@@ -0,0 +1,11 @@
+package com.example.giris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YeniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yeni)
+    }
+}
