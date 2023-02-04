25d8c49d83dbe6eb8aba85bd1bef55f3a33903f6
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2023-02-04 20:43:51 +0300

    Kotlin 1.5

---
+++ AndroidCalismaYapisi/SayfalarArasiGecis/app/src/main/java/com/example/sayfalararasigecis/ActivityC.kt
@@ -0,0 +1,17 @@
+package com.example.sayfalararasigecis
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
+    }
+
+    override fun onBackPressed() {
+        val yeniIntent = Intent(this@ActivityC,MainActivity::class.java)
+        startActivity(yeniIntent)
+    }
+}
