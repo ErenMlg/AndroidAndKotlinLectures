6ba80394768a4e428ea85e504102cd623d9ae1c6
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2023-03-06 12:08:49 +0300

    Kotlin 2.3

--- MaterialDesign/Toolbar/app/src/main/java/com/example/ask/toolbar/MainActivity.kt
+++ MaterialDesign/Toolbar/app/src/main/java/com/example/ask/toolbar/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.ask.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ask.toolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var design:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        design = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(design.root)
+
+        design.toolbar.title = "Özel Toolbar Başlığı"
+        design.toolbar.subtitle = "Özel Toolbar AltBaşlığı"
+        design.toolbar.setLogo(R.drawable.resim)
+        design.toolbar.setTitleTextColor(resources.getColor(R.color.purple_700))
+        // resources.getColor Fonksiyonu color dosyasına erişmemizi sağlıyor.
+
+        setSupportActionBar(design.toolbar)
     }
 }
