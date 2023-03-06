fe0723ba9509a29b7df5115d6f2c8914d4190c30
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2023-03-06 12:43:28 +0300

    Kotlin 2.3

--- MaterialDesign/Toolbar/app/src/main/java/com/example/ask/toolbar/MainActivity.kt
+++ MaterialDesign/Toolbar/app/src/main/java/com/example/ask/toolbar/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.ask.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.ask.toolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +23,34 @@ class MainActivity : AppCompatActivity() {
         // resources.getColor Fonksiyonu color dosyasına erişmemizi sağlıyor.
 
         setSupportActionBar(design.toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+
+            R.id.action_bilgi -> {
+               Toast.makeText(this@MainActivity,"Bilgi",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_ayarlar -> {
+               Toast.makeText(this@MainActivity,"Ayarlar",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_ekle -> {
+               Toast.makeText(this@MainActivity,"Eklendi",Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_çikis -> {
+               finish()
+                return true
+            }
+            else -> {
+                return true
+            }
+        }
     }
 }
