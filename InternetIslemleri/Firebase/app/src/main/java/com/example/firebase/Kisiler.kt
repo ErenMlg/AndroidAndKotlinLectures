ee23c6247e7aaf25541ce0fee2c8cd328c666f71
Eren Mollaoğlu <eren.mollaoglu@outlook.com>
2023-04-06 13:29:59 +0300

    Kotlin 2.7

--- InternetIslemleri/Firebase/app/src/main/java/com/example/firebase/Kisiler.kt
+++ InternetIslemleri/Firebase/app/src/main/java/com/example/firebase/Kisiler.kt
@@ -3,5 +3,4 @@ package com.example.firebase
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class Kisiler(var kisi_ad: String? = "", var kisi_yas: Int? = 0) {
-}
+data class Kisiler(var kisi_ad: String? = "", var kisi_yas: Int? = 0)
--- InternetIslemleri/Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
+++ InternetIslemleri/Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -21,23 +26,64 @@ class MainActivity : AppCompatActivity() {
         //kisiSil("-NSIEdT8WrjDdGheOgBL")
 
         //kisiGuncelle("-NSIEI8MWruZJ5n9zj-M")
-
+        //tumKisiler()
+        kisiAra()
     }
 
-    fun kisiEkle(ad:String, yas:Int){
-        val kisi = Kisiler(ad,yas)
+    fun kisiEkle(ad: String, yas: Int) {
+        val kisi = Kisiler(ad, yas)
         refKisiler.push().setValue(kisi)
     }
 
-    fun kisiSil(key:String){
+    fun kisiSil(key: String) {
         refKisiler.child(key).removeValue()
     }
 
-    fun kisiGuncelle(key:String){
-        val updateInfo = HashMap<String,Any>()
-        updateInfo["kisi_ad"]="Çiğdem Oğurlu"
-        updateInfo["kisi_yas"]=43
+    fun kisiGuncelle(key: String) {
+        val updateInfo = HashMap<String, Any>()
+        updateInfo["kisi_ad"] = "Çiğdem Oğurlu"
+        updateInfo["kisi_yas"] = 43
         refKisiler.child(key).updateChildren(updateInfo)
+    }
+
+    fun tumKisiler() {
+        refKisiler.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (kisiler in snapshot.children) {
+                    val kisi = kisiler.getValue(Kisiler::class.java)
+                    if (kisi != null) {
+                        val key = kisiler.key // Elemanın keyini aldığımız kodlama
+                        Log.e("Kişi :", "${kisi.kisi_ad} Yaş :${kisi.kisi_yas}")
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    private fun kisiAra() {
+
+        // var sorgu = refKisiler.limitToFirst(1) -> Sınırlı sayıda veri almak için
+        // var sorgu = refKisiler.limitToLast(2) -> Sondan iki veriyi getirir
+        // var sorgu = refKisiler.orderByChild("kisi_yas").startAt(30.0).endAt(50.0) -> yaşı 30 ile 50 olan kişiler
+
+        var sorgu = refKisiler.orderByChild("kisi_ad").equalTo("Eren")
+        sorgu.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (gelenVeri in snapshot.children) {
+                    val veri = gelenVeri.getValue(Kisiler::class.java)
+                    if (veri != null) {
+                        Log.e("Kişi :", "${veri.kisi_ad} Yaş :${veri.kisi_yas}")
+                    } else {
+                        Log.e("Hata", "Null")
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
     }
 
 }
